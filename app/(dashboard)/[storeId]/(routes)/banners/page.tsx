39e2a45a5a804a2bd9a9852e3f0687d65de5512a
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2025-01-08 20:25:51 +0700

    Add Banner Pages

---
+++ app/(dashboard)/[storeId]/(routes)/banners/page.tsx
@@ -0,0 +1,11 @@
+import { BannerClient } from "./components/client";
+
+const BannersPage = () => {
+  return (
+    <div>
+      <BannerClient />
+    </div>
+  );
+};
+
+export default BannersPage;
