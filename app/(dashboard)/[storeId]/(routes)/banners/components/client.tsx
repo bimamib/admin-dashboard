ac1cb1bafa75297f63993e1f1dcddcebad7f7357
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2025-01-08 20:33:22 +0700

    Feat Banner

--- app/(dashboard)/[storeId]/(routes)/banners/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/banners/components/client.tsx
@@ -1,5 +1,19 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { Heading } from "@/components/ui/heading";
+import { Plus } from "lucide-react";
+
 export const BannerClient = () => {
-  return <div>Banner Client</div>;
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading title="Banner (0)" description="Atur Banner Untuk Tokomu" />
+        <Button>
+          <Plus className="mr-2 h-4 w-4" />
+          Add New
+        </Button>
+      </div>
+    </>
+  );
 };
