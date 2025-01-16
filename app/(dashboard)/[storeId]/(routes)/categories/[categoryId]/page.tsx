d74a84d45f95e48195eaa0b29a22ef596f029701
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2025-01-16 16:34:23 +0700

    Add Function Categories route

---
+++ app/(dashboard)/[storeId]/(routes)/categories/[categoryId]/page.tsx
@@ -0,0 +1,20 @@
+import db from "@/lib/db";
+import { BannerForm } from "./components/category-form";
+
+const BannerPage = async ({ params }: { params: { bannerId: string } }) => {
+  const banner = await db.banner.findUnique({
+    where: {
+      id: params.bannerId,
+    },
+  });
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <BannerForm initialData={banner} />
+      </div>
+    </div>
+  );
+};
+
+export default BannerPage;
